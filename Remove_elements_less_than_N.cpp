337040cbd1951340f9a9653254807b9138dd98ec
SedaMirzoyan <135509078+SedaMirzoyan@users.noreply.github.com>
2023-08-18 10:34:59 +0400

    Add files via upload

---
+++ Remove_elements_less_than_N.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+#include <list>
+#include <vector>
+#include <set>
+
+
+unsigned int n = 23;
+
+
+bool lessThanN(unsigned int num)
+{
+    return num < n;
+}
+
+void removeFromList(std::list<int> l)
+{
+    std::list<int>::iterator it;
+	
+	l.remove_if(lessThanN); //case #1//  -5, 23, 34, 45, 56, 67, 78, 23, 89, -8, 94, 101, 23
+    
+	remove_if(l.begin(), l.end(), lessThanN); //case #2// -5, 23, 34, 45, 56, 67, 78, 23, 89, -8, 94, 101, 23 , -8, 94, 101, 23, 3
+
+
+    for ( it = l.begin(); it != l.end(); ++it)
+    {
+        std::cout << *it << std::endl;
+    }
+}
+
+
+int main()
+{
+    std::list<int> l{ 11,-5, 2, 3, 23,34,45,56,67,78, 23, 89, 3,-8, 94, 101, 23, 3 };
+    removeFromList(l);
+
+
+    return 0;
+}
