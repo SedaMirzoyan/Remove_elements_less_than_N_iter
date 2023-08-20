f44f12bd840d1bca3b31e48a6d6f51a359e84b8f
SedaMirzoyan <135509078+SedaMirzoyan@users.noreply.github.com>
2023-08-21 00:15:45 +0400

    Add files via upload

--- Remove_elements_less_than_N.cpp
+++ Remove_elements_less_than_N.cpp
@@ -4,36 +4,96 @@
 #include <vector>
 #include <set>
 
+int n = 23;
 
-unsigned int n = 23;
-
-
-bool lessThanN(unsigned int num)
-{
-    return num < n;
-}
-
-void removeFromList(std::list<int> l)
+void removeFromList(std::list<int> l)   // #68
 {
     std::list<int>::iterator it;
-	
-	l.remove_if(lessThanN); //case #1//  -5, 23, 34, 45, 56, 67, 78, 23, 89, -8, 94, 101, 23
-    
-	remove_if(l.begin(), l.end(), lessThanN); //case #2// 23, 34, 45, 56, 67, 78, 23, 89, 94, 101, 23 , 89, 3, -8, 94, 101, 23, 3
 
-
-    for ( it = l.begin(); it != l.end(); ++it)
+    for (it = l.begin(); it != l.end(); )
     {
-        std::cout << *it << std::endl;
+        if (*it < n)
+        {
+            it = l.erase(it);
+        }
+        else
+        {
+            ++it;
+        }
+    }
+
+    for (it = l.begin(); it != l.end(); ++it)
+    {
+        std::cout << *it << " ";
+    }
+}
+
+
+void removeFromVec(std::vector<int> vec)    // #69
+{
+    std::vector<int>::iterator it;
+
+    for (it = vec.begin(); it != vec.end(); )
+    {
+        if (*it < n)
+        {
+            it = vec.erase(it);
+        }
+        else
+        {
+            ++it;
+        }
+    }
+
+    for (it = vec.begin(); it != vec.end(); ++it)
+    {
+        std::cout << *it << " ";
+    }
+}
+
+
+void removeFromSet(std::set<int> s)    // #70
+{
+    std::set<int>::iterator it;
+
+    for (it = s.begin(); it != s.end(); )
+    {
+        if (*it < n)
+        {
+            it = s.erase(it);
+        }
+        else
+        {
+            ++it;
+        }
+    }
+
+    for (it = s.begin(); it != s.end(); ++it)
+    {
+        std::cout << *it << " ";
     }
 }
 
 
 int main()
 {
+    //List
+    std::cout << "List elements\n";
     std::list<int> l{ 11,-5, 2, 3, 23,34,45,56,67,78, 23, 89, 3,-8, 94, 101, 23, 3 };
     removeFromList(l);
+    std::cout << std::endl;
 
+    //Vector
+    std::cout << "Vector elements\n"; 
+    std::vector<int> vec{ 3,-21, 567, -1, 23, 98, 105, -21, 73, 104, 23 };
+    removeFromVec(vec);
+    std::cout << std::endl;
+
+    //Set
+    std::cout << "Set elements\n";
+    std::set<int> s{ 53,41, -21, 512, -10, 23, 98, 0, 105, 712, 194, 15, 7, 45 };
+    removeFromSet(s);
+    std::cout << std::endl;
 
     return 0;
 }
